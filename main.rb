70d2c5ba610df476ba35ffce7993f2fe62530243
Micah <micahincorporated@gmail.com>
2018-09-12 08:34:31 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
+# Welcome message
 puts "Hello!  Welcome to TextCalc 0.1"
+# Define calculator function
 def runcalc
+# Ask for operator
 puts "Add, subtract, or multiply?"
 opor= gets.chomp
 if opor == "add" then 
+# Adding
 puts "1st Number"
 frst= gets.to_i
 puts "2nd Number"
@@ -11,6 +15,7 @@ puts "Calculating..."
 puts frst + scnd
 puts "Thank you for using the calc.  Type /stop to end the calculator"
 elsif opor == "subtract"
+# Subtracting
 puts "1st Number"
 frst= gets.to_i
 puts "2nd Number"
@@ -19,6 +24,7 @@ puts "Calculating..."
 puts frst - scnd
 puts "Thank you for using the calc.  Type /stop to end the calculator"
 elsif opor == "multiply"
+# Multiplying
 puts "1st Number"
 frst= gets.to_i
 puts "2nd Number"
@@ -26,10 +32,19 @@ scnd= gets.to_i
 puts "Calculating..."
 puts frst * scnd
 puts "Thank you for using the calc.  Type /stop to end the calculator or /again to run it again."
-end
+elsif opor == "divide"
+# Dividing
+puts "1st Number"
+frst= gets.to_i
+puts "2nd Number"
+scnd= gets.to_i
+puts "Calculating..."
+puts frst * scnd
+puts "Thank you for using the calc.  Type /stop to end the calculator or /again to run it again."
+end # Ends the if block
 strp= gets.chomp
 if strp=="/stop" then puts "Stopped." 
 elsif strp == "/again" then runcalc
 end
 end
-runcalc
+runcalc # Starts the program.
