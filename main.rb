8c9d8ec2dab537bea3829d79e50545381b2c4f1c
Micah <micahincorporated@gmail.com>
2018-09-12 08:03:54 -0500

    0.3 Impeccable Remarkable System
    Added support for subtraction and removed useless code.

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
 puts "Hello!  Welcome to TextCalc 0.1"
+puts "Add or subtract?"
+opor= gets.chomp
+if opor == "add" then 
 puts "1st Number"
 frst= gets.to_i
 puts "2nd Number"
@@ -6,5 +9,14 @@ scnd= gets.to_i
 puts "Calculating..."
 puts frst + scnd
 puts "Thank you for using the calc.  Type /stop to end the calculator"
+else
+puts "1st Number"
+frst= gets.to_i
+puts "2nd Number"
+scnd= gets.to_i
+puts "Calculating..."
+puts frst - scnd
+puts "Thank you for using the calc.  Type /stop to end the calculator"
+end
 strp= gets.chomp
 if strp=="/stop" then puts "Stopped." end
