c51ae7a0dbb5f4e2d06d0be7cd45fbc52c8c56dd
Micah <micahincorporated@gmail.com>
2018-09-13 13:57:42 -0500

    0.7.2 Light Verifiable Spry Keychain

--- newcalc.rb
+++ newcalc.rb
@@ -1,5 +1,34 @@
 # Welcome
 puts "TextCalC Light 0.71, forked from @micahlt"
+def runit
 puts "Enter expression"
-prob = gets.to_i
-puts prob
+prob = gets
+prob = prob.split
+if prob[1] == "*" then
+  puts prob[0].to_i * prob[2].to_i
+    if gets.chomp == "again" then
+      runit
+    else puts "Done." end
+  elsif prob[1] == "/" then
+  puts prob[0].to_i / prob[2].to_i
+    if gets.chomp == "again" then
+      runit
+    else puts "Done." end
+  elsif prob[1] == "+" then
+  puts prob[0].to_i + prob[2].to_i
+    if gets.chomp == "again"
+      runit
+    else puts "Done." end
+  elsif prob[1] == "-" then
+  puts prob[0].to_i - prob[2].to_i
+    if gets.chomp == "again"
+      runit
+    else puts "Done." end
+  elsif prob[1] == "ex" then
+  puts prob[0].to_i ** prob[2].to_i
+    if gets.chomp == "again" then
+      runit
+    else puts "Done." end
+end
+end
+runit
